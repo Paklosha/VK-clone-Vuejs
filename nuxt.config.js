13e345d9f5012d2cf007078a8e69f5f374e8736b
Paklosha <paklosha@gmail.com>
2020-10-19 09:05:17 -0700

    axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,21 @@ module.exports = {
   '@nuxtjs/axios',
   '@nuxtjs/auth'
 ],
+axios: {
+  baseURL: 'http://localhost:3000', // Used as fallback if no runtime config is provided
+},
+
+publicRuntimeConfig: {
+  axios: {
+    browserBaseURL: process.env.BROWSER_BASE_URL
+  }
+},
+
+privateRuntimeConfig: {
+  axios: {
+    baseURL: process.env.BASE_URL
+  }
+},
 auth: {
   strategies: {
     local: {
